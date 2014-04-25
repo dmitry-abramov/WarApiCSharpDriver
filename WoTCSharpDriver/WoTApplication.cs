f11931419cbf723246facb739f7c3725c1fa4a9d
gollazio <abramov.dmitry92@gmail.com>
2014-04-25 19:05:35 +0400

    Added WoTApplication class
    Added WoTApplication class

---
+++ WoTCSharpDriver/WoTApplication.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WoTCSharpDriver
+{
+    public class WoTApplication
+    {
+        public string ApplicationId { get; private set; }
+
+        public WoTApplication(string applicationId)
+        {
+            ApplicationId = applicationId;
+        }
+
+        public RequestBase CreateRequest<TRequest>() where TRequest : RequestBase, new()
+        {
+            var request = new TRequest();
+            request.ApplicationId = ApplicationId;
+            return request;
+        }
+    }
+}

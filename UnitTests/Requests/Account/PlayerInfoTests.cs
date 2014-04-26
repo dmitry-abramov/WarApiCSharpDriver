6a1b3684afb3296e8c2ea1ffadb1a8846fbff8b0
gollazio <abramov.dmitry92@gmail.com>
2014-04-26 13:16:00 +0400

    Added unit tests

---
+++ UnitTests/Requests/Account/PlayerInfoTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WoTCSharpDriver.Requests.Account;
+
+namespace UnitTests.Requests.Account
+{
+    [TestClass]
+    public class PlayerInfoTests
+    {
+        [TestMethod]
+        public void GetPath()
+        {
+            var request = new PlayerInfo();
+            var path = request.GetPath();
+
+            Assert.AreEqual("account/info", path);
+        }
+    }
+}

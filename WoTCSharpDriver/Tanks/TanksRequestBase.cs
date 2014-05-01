c4b27906cc94a5ef53e065900bd6dd57582d0440
gollazio <abramov.dmitry92@gmail.com>
2014-05-01 12:01:14 +0400

    Addeds tanks group requests

---
+++ WoTCSharpDriver/Tanks/TanksRequestBase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using WoTCSharpDriver.Requests;
+using WoTCSharpDriver.Attributes;
+
+namespace WoTCSharpDriver.Tanks
+{
+    public class TanksRequestBase : RequestBase
+    {
+        public override string MethodBlock
+        {
+            get
+            {
+                return "tanks";
+            }
+        }
+
+        [RequestParameter("fields", false)]
+        public string Fields { get; set; }
+
+        [RequestParameter("account_id", true)]
+        public string AccountId { get; set; }
+
+        [RequestParameter("tank_id", false)]
+        public string TankId { get; set; }
+
+        [RequestParameter("in_garage", false)]
+        public int InGarage { get; set; }
+    }
+}
---
+++ WoTCSharpDriver/Tanks/TanksStatisticsRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WoTCSharpDriver.Tanks
+{
+    public class TanksStatisticsRequest : TanksRequestBase
+    {
+        public override string MethodName
+        {
+            get
+            {
+                return "stats";
+            }
+        }
+    }
+}

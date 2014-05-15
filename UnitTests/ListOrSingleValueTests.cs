d4241be68796bff497af0b5a353cdf1b43d607a3
gollazio <abramov.dmitry92@gmail.com>
2014-05-15 22:45:39 +0400

    Added generic class for list/singel values properties of requests with tests

---
+++ UnitTests/ListOrSingleValueTests.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WoTCSharpDriver;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class ListOrSingleValueTests
+    {
+        [TestMethod]
+        public void TestObjectInitialization()
+        {
+            var o = new ListOrSingleValue<int>() { 1, 2, 3 };
+
+            Assert.AreEqual(3, o.Count);
+            Assert.IsFalse(o.IsSingle);
+            Assert.AreEqual(1, o.ElementAt(0));
+            Assert.AreEqual(2, o.ElementAt(1));
+            Assert.AreEqual(3, o.ElementAt(2));
+        }
+
+        [TestMethod]
+        public void TestIsSingleProperty()
+        {
+            var o = new ListOrSingleValue<int>();
+
+            o.Add(5);
+
+            Assert.AreEqual(1, o.Count);
+            Assert.IsTrue(o.IsSingle);
+            Assert.AreEqual(5, o.ElementAt(0));
+        }
+
+        [TestMethod]
+        public void TestImplicitCastFromSingleInt()
+        {
+            ListOrSingleValue<int> o = 5;
+
+            Assert.AreEqual(1, o.Count);
+            Assert.IsTrue(o.IsSingle);
+            Assert.AreEqual(5, o.ElementAt(0));
+        }
+
+        [TestMethod]
+        public void TestImplicitCastFromListInt()
+        {
+            var list = new List<int> { 10, 12, 15 };
+            ListOrSingleValue<int> o = list;
+
+            Assert.AreEqual(3, o.Count);
+            Assert.IsFalse(o.IsSingle);
+            Assert.AreEqual(10, o.ElementAt(0));
+            Assert.AreEqual(12, o.ElementAt(1));
+            Assert.AreEqual(15, o.ElementAt(2));
+        }
+
+        [TestMethod]
+        public void TestImplicitCastToSingleInt()
+        {
+            ListOrSingleValue<int> o = 7;
+            int value = o;
+
+            Assert.AreEqual(7, value);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(InvalidCastException))]
+        public void TestImplicitCastToSingleIntForListCase()
+        {
+            var o = new ListOrSingleValue<int>() { 1, 2, 3 };
+            int value = o;
+        }
+
+        [TestMethod]
+        public void TestToStringMethodForSingleInt()
+        {
+            ListOrSingleValue<int> o = 8;
+
+            Assert.AreEqual(o.ToString(), "8");
+        }
+
+        [TestMethod]
+        public void TestToStringMethodForListCase()
+        {
+            var o = new ListOrSingleValue<int>() { 1, 2, 3 };
+
+            Assert.AreEqual(o.ToString(), "1,2,3");
+        }
+
+        [TestMethod]
+        public void TestToStringMethodForEmptyCase()
+        {
+            var o = new ListOrSingleValue<int>();
+
+            Assert.AreEqual(o.ToString(), string.Empty);
+        }
+    }
+}
---
+++ WoTCSharpDriver/ListOrSingleValue.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WoTCSharpDriver
+{
+    public class ListOrSingleValue<TValue> : IList<TValue>
+    {
+        private List<TValue> data;
+
+        public ListOrSingleValue()
+        {
+            data = new List<TValue>();
+        }
+
+        public ListOrSingleValue(IEnumerable<TValue> values)
+        {
+            data = new List<TValue>(values);
+        }
+
+        public bool IsSingle
+        {
+            get
+            {
+                return this.Count == 1;
+            }
+        }
+
+        public static implicit operator TValue(ListOrSingleValue<TValue> listOrSingleValue)
+        {
+            if (listOrSingleValue.IsSingle)
+            {
+                return listOrSingleValue.ElementAt(0);
+            }
+
+            throw new InvalidCastException(string.Format("Object is list with {0} elements. It can be cast to single element", listOrSingleValue.Count));
+        }
+
+        public static implicit operator ListOrSingleValue<TValue>(TValue value)
+        {
+            return new ListOrSingleValue<TValue>() { value };
+        }
+
+        public static implicit operator ListOrSingleValue<TValue>(List<TValue> values)
+        {
+            return new ListOrSingleValue<TValue>(values);
+        }
+
+        public override string ToString()
+        {
+            return string.Join(",", this);
+        }
+
+        public int IndexOf(TValue item)
+        {
+            return data.IndexOf(item);
+        }
+
+        public void Insert(int index, TValue item)
+        {
+            data.Insert(index, item);
+        }
+
+        public void RemoveAt(int index)
+        {
+            data.RemoveAt(index);
+        }
+
+        public TValue this[int index]
+        {
+            get
+            {
+                return data[index];
+            }
+            set
+            {
+                data[index] = value;
+            }
+        }
+
+        public void Add(TValue item)
+        {
+            data.Add(item);
+        }
+
+        public void Clear()
+        {
+            data.Clear();
+        }
+
+        public bool Contains(TValue item)
+        {
+            return data.Contains(item);
+        }
+
+        public void CopyTo(TValue[] array, int arrayIndex)
+        {
+            data.CopyTo(array, arrayIndex);
+        }
+
+        public int Count
+        {
+            get
+            {
+                return data.Count;
+            }
+        }
+
+        public bool IsReadOnly
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public bool Remove(TValue item)
+        {
+            return data.Remove(item);
+        }
+
+        public IEnumerator<TValue> GetEnumerator()
+        {
+            return data.GetEnumerator();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return data.GetEnumerator();
+        }
+    }
+}

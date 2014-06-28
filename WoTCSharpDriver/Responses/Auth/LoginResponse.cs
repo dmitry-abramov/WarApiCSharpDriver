91f6360845dcd477c869b66ac0a5aed60cc8f9a1
gollazio <abramov.dmitry92@gmail.com>
2014-06-28 22:36:07 +0400

    added requests for auth responses

---
+++ WoTCSharpDriver/Responses/Auth/LoginResponse.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.Serialization;
+
+namespace WoTCSharpDriver.Responses.Auth
+{
+    [DataContract]
+    public class LoginResponse
+    {
+        [DataMember(Name = "location")]
+        public string Location { get; set; }
+    }
+}
---
+++ WoTCSharpDriver/Responses/Auth/ProlongateResponse.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.Serialization;
+
+namespace WoTCSharpDriver.Responses.Auth
+{
+    [DataContract]
+    public class ProlongateResponse
+    {
+        [DataMember(Name = "access_token")]
+        public string AccessToken { get; set; }
+
+        [DataMember(Name = "account_id")]
+        public string AccountId { get; set; }
+
+        // todo: use date time
+        [DataMember(Name = "expires_at")]
+        public string ExpiresAt { get; set; }
+    }
+}

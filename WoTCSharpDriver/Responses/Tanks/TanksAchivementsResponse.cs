479cf58f70f73bf2319aaf9cf5ed9e2da7278d87
gollazio <abramov.dmitry92@gmail.com>
2014-06-29 12:27:31 +0400

    added responses for tanks statistics requests

---
+++ WoTCSharpDriver/Responses/Tanks/TanksAchivementsResponse.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace WoTCSharpDriver.Responses.Tanks
+{
+    [DataContract]
+    public class TanksAchivementsData
+    {
+        [DataMember(Name = "account_id")]
+        public string AccountId { get; set; }
+
+        [DataMember(Name = "TankId")]
+        public string TankId { get; set; }
+
+        [DataMember(Name = "series")]
+        public string Series { get; set; }
+
+        [DataMember(Name = "max_series")]
+        public string MaxSeries { get; set; }
+
+        [DataMember(Name = "achivements")]
+        public string Achivements { get; set; }
+    }
+
+    [DataContract]
+    public class TanksAchivementsResponse : ResponseBase<IDictionary<string, TanksAchivementsData>>
+    { 
+    }
+}
---
+++ WoTCSharpDriver/Responses/Tanks/TanksStatisticsResponse.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace WoTCSharpDriver.Responses.Tanks
+{
+    [DataContract]
+    public class TanksStatisticsData
+    {
+        [DataMember(Name = "account_id")]
+        public string AccountId { get; set; }
+
+        // todo: use enum
+        [DataMember(Name = "mark_of_mastery")]
+        public int AccountId { get; set; }
+
+        [DataMember(Name = "max_frags")]
+        public int MaxFrags { get; set; }
+
+        [DataMember(Name = "max_xp")]
+        public int MaxExpirience { get; set; }
+
+        [DataMember(Name = "tank_id")]
+        public string TankId { get; set; }
+
+        [DataMember(Name = "in_garage")]
+        public bool InGarage { get; set; }
+
+        [DataMember(Name = "all")]
+        public TankStatisticDetails All { get; set; }
+
+        [DataMember(Name = "clan")]
+        public TankStatisticDetails Clan { get; set; }
+
+        [DataMember(Name = "company")]
+        public TankStatisticDetails Company { get; set; }
+    }
+
+    [DataContract]
+    public class TankStatisticDetails
+    {
+        [DataMember(Name = "battle_avg_xp")]
+        public int BattleAverengeExpirience { get; set; }
+
+        [DataMember(Name = "battles")]
+        public int Battles { get; set; }
+
+        [DataMember(Name = "capture_points")]
+        public int CapturePoints { get; set; }
+
+        [DataMember(Name = "damage_dealt")]
+        public int DamageDealt { get; set; }
+
+        [DataMember(Name = "damage_received")]
+        public int DamageReceived { get; set; }
+
+        [DataMember(Name = "draws")]
+        public int Draws { get; set; }
+
+        [DataMember(Name = "dropped_capture_points")]
+        public int DroppedCapturePoints { get; set; }
+
+        [DataMember(Name = "frags")]
+        public int Frags { get; set; }
+
+        [DataMember(Name = "hits")]
+        public int Hits { get; set; }
+
+        [DataMember(Name = "hits_percents")]
+        public int HitsPercents { get; set; }
+
+        [DataMember(Name = "losses")]
+        public int Losses { get; set; }
+
+        [DataMember(Name = "shots")]
+        public int Shots { get; set; }
+
+        [DataMember(Name = "spotted")]
+        public int Spotted { get; set; }
+
+        [DataMember(Name = "survived_battles")]
+        public int SurvivedBattles { get; set; }
+
+        [DataMember(Name = "wins")]
+        public int Wins { get; set; }
+
+        [DataMember(Name = "xp")]
+        public int Expirience { get; set; }
+    }
+
+    [DataContract]
+    public class TanksStatisticsResponse : ResponseBase<IDictionary<string, TanksStatisticsData>>
+    {
+    }
+}

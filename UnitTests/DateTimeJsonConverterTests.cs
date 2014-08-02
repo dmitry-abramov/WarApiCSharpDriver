5691de91c33f64ae089b25b329329efc2e164283
gollazio <abramov.dmitry92@gmail.com>
2014-08-02 20:47:09 +0400

    added DateTimeJsonConverter

---
+++ UnitTests/DateTimeJsonConverterTests.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using System;
+using Utilities.Serialization;
+using WarApiCSharpDriver.Serialization;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class DateTimeJsonConverterTests
+    {
+        private ISerializer serializer;
+
+        public ISerializer Serializer
+        {
+            get
+            {
+                if (serializer == null)
+                {
+                    var serializerSettings = new JsonSerializerSettings();
+                    serializerSettings.Converters.Add(new DateTimeJsonConverter());
+
+                    serializer = new NewtonsoftSerializer(serializerSettings);
+                }
+
+                return serializer;
+            }
+        }
+
+        [TestMethod]
+        public void TestDeserialize()
+        {
+            var serializedValue = "1313397750";
+
+            var deserializedValue = Serializer.Deserialize<DateTime>(serializedValue);
+
+            var expectedValue = new DateTime(2011, 8, 15, 8, 42, 30);
+            Assert.AreEqual(expectedValue, deserializedValue);
+        }
+
+        [TestMethod]
+        public void TestDeserializeAsObject()
+        {
+            var serializedValue = "1313397750";
+
+            var deserializedValue = Serializer.Deserialize(serializedValue);
+
+            Int64 expectedValue = 1313397750;
+            Assert.AreEqual(expectedValue, deserializedValue);
+        }
+
+        [TestMethod]
+        public void TestSerialize()
+        {
+            var date = new DateTime(2011, 8, 15, 8, 42, 30); 
+            var expectedValue = "1313397750";
+
+            var serializedValue = Serializer.Serialize(date);
+
+            Assert.AreEqual(expectedValue, serializedValue);
+        }
+
+        [TestMethod]
+        public void TestSerializeDateBeforeEpoch()
+        {
+            var date = new DateTime(1957, 10, 4, 0, 0, 0);
+            var expectedValue = "-386380800";
+
+            var serializedValue = Serializer.Serialize(date);
+
+            Assert.AreEqual(expectedValue, serializedValue);
+        }
+
+        [TestMethod]
+        public void TestDeserializeDateBeforeEpoch()
+        {
+            var serializedValue = "-386380800";
+
+            var deserializedValue = Serializer.Deserialize<DateTime>(serializedValue);
+
+            var expectedValue = new DateTime(1957, 10, 4, 0, 0, 0);
+            Assert.AreEqual(expectedValue, deserializedValue);
+        }
+    }
+}
---
+++ Utilities/Serialization/DateTimeJsonConverter.cs
@@ -0,0 +1,54 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+
+namespace WarApiCSharpDriver.Serialization
+{
+    public class DateTimeJsonConverter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(DateTime);
+        }
+
+        public override object ReadJson(JsonReader reader, System.Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType != JsonToken.Integer)
+            {
+                throw new FormatException(
+                    String.Format("Unexpected token parsing date. Expected Integer, got {0}.",
+                    reader.TokenType));
+            }
+
+            var ticks = (long)reader.Value;
+
+            var date = new DateTime(1970, 1, 1);
+            date = date.AddSeconds(ticks);
+
+            return date;
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            long ticks;
+            if (value is DateTime)
+            {
+                var epoc = new DateTime(1970, 1, 1);
+                var delta = ((DateTime)value) - epoc;
+                
+                /*if (delta.TotalSeconds < 0)
+                {
+                    throw new ArgumentOutOfRangeException(
+                        "Unix epoc starts January 1st, 1970");
+                }*/
+
+                ticks = (long)delta.TotalSeconds;
+            }
+            else
+            {
+                throw new Exception("Expected DateTime object.");
+            }
+            writer.WriteValue(ticks);
+        }
+    }
+}

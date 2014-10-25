50f95b64438649baedfa8e205812d7db141d1eef
dmitry-abramov <abramov.dmitry92@gmail.com>
2014-10-25 14:13:56 +0400

    task8 created projects for planes and ships with structure and client classes

---
+++ WarApi.WoWPCSharpDriver/WoWPApplication.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using WarApi.Client;
+using WarApi.Utilities.Serialization;
+
+namespace WarApi.WoWPCSharpDriver
+{
+    public class WoWPApplication : WarApiClientBase
+    {
+        public WoWPApplication(string applicationId) 
+            : this(applicationId, new NewtonsoftSerializer())
+        {            
+        }
+
+        public WoWPApplication(string applicationId, ISerializer serializer)
+            : this(applicationId, "api.worldofwarplanes.ru", "wowp", serializer)
+        {
+        }
+
+        public WoWPApplication(string applicationId, string server, string apiName) 
+            : this(applicationId, server, apiName, new NewtonsoftSerializer())
+        {
+        }
+
+        public WoWPApplication(string applicationId, string server, string apiName, ISerializer serializer)
+            : base(applicationId, server, apiName, serializer)
+        {
+        }
+    }
+}
---
+++ WarApi.WoWSCSharpDriver/WoWSApplication.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using WarApi.Client;
+using WarApi.Utilities.Serialization;
+
+namespace WarApi.WoWSCSharpDriver
+{
+    public class WoWSApplication : WarApiClientBase
+    {
+        public WoWSApplication(string applicationId) 
+            : this(applicationId, new NewtonsoftSerializer())
+        {            
+        }
+
+        public WoWSApplication(string applicationId, ISerializer serializer)
+            : this(applicationId, "api.worldofwarships.ru", "wows", serializer)
+        {
+        }
+
+        public WoWSApplication(string applicationId, string server, string apiName) 
+            : this(applicationId, server, apiName, new NewtonsoftSerializer())
+        {
+        }
+
+        public WoWSApplication(string applicationId, string server, string apiName, ISerializer serializer)
+            : base(applicationId, server, apiName, serializer)
+        {
+        }
+    }
+}
